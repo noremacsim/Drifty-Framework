90d6dbce131a4c3dffbecf2e86eaf31102b61843
noremacsim <mrcameronsim@gmail.com>
2021-10-13 21:21:03 +0100

    [FrameWork] - Styling

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['page'] = array(
+    'title' => 'Drifty Framework',
+    'description' => 'The Lightweight PHP Framewor',
+    'background' => 'bg-gray-100',
+    'topNav' => false,
+);
