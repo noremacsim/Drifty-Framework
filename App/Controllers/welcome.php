df25e7984f69f7d7bbb2e0bc4ff058bb4e5ae024
noremacsim <mrcameronsim@gmail.com>
2022-02-15 00:09:16 +0000

    Fix - Remove unnecessary controller line

--- App/Controllers/welcome.php
+++ App/Controllers/welcome.php
@@ -2,11 +2,6 @@
 namespace Drifty\Controllers;
 
 class welcome extends controller {
-    protected $config = [
-      'modal' => 'welcome',
-    ];
-    protected $primaryKey = 'id';
-
     public function welcome()
     {
         return $this->render('welcome.tpl');
